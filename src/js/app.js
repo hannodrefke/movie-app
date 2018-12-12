df042efbf363a8aec73c09a1c0356f8e66029ae9
hannodrefke <info@born83.com>
2018-12-12 16:19:45 +0100

    Add bootstrap and font-awesome

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 
 import App from './components/App';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 import '../css/style.scss';
 
 render(<App />, document.querySelector('#main'));
